dc2c18bea1f83679e678d475d817d32ee2d08317
Soami Charan <charansoami@gmail.com>
2019-06-24 14:58:30 +0530

    Update admin panel style and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 User.create(:email => "charansoami@gmail.com ", :password => "soami",:admin => true)
 User.create(:email => "mukul@kaushik", :password => "kaushik",:admin => true)
+User.create(:email => "blazar@solution", :password => "blazar", :admin => true)
